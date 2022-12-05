0fba5aff94438ae31ab28477041f949bfc002701
LoyalPotato <david.bazilio.r@gmail.com>
2022-12-05 23:38:11 +0000

    Add mdx support to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://loyalpotato.dev/",
   integrations: [
+    mdx(),
     sitemap(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
