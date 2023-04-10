3dd8590f99b68d60d4e99e3035b710a0a3993a8f
LoyalPotato <david.bazilio.r@gmail.com>
2023-04-10 14:15:09 +0100

    Remove dupe comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
