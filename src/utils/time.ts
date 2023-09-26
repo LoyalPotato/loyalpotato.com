8c7bea056de48b7feed91e0e38aac49f410c823f
LoyalPotato <=>
2023-09-26 07:47:11 +0100

    Refactor time format into util

---
+++ src/utils/time.ts
@@ -0,0 +1,8 @@
+export const timeFormat_2M_2Y: Intl.DateTimeFormatOptions = {
+  month: "2-digit",
+  year: "2-digit",
+};
+
+export const timeFormat_long: Intl.DateTimeFormatOptions = {
+  dateStyle: "long",
+};
