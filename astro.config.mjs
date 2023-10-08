3421c4b70ae9305b684a403cbc4730479d602862
LoyalPotato <=>
2023-10-08 10:35:24 +0100

    Add astro-robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import robots from "astro-robots";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://loyalpotato.dev/",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), robots()]
 });
